1ba61a676be7483a55d121fe4060d5bc4a05f984
shaurya0406 <shauryachandra0406@gmail.com>
2024-08-11 11:48:47 +0530

    [UPDATE] Added Trap Handling logic for WB stage

--- rtl/asrv32_writeback.v
+++ rtl/asrv32_writeback.v
@@ -9,11 +9,18 @@ module asrv32_writeback #(parameter PC_RESET = 32'h00_00_00_00) (
     /* Inputs */
     input wire i_clk, i_rst_n,
     input wire i_writeback_en,                  // Enable o_wr_rd_en if stage is currently on WRITEBACK Stage
+    input wire[2:0] i_funct3,                   // Function type 
     input wire[31:0] i_result_from_alu,         // Output of ALU
     input wire[31:0] i_imm,                     // Immediate value
     input wire[31:0] i_rs1_data,                // Source register 1 value
     input wire[31:0] i_load_data_from_memory,   // Data to be loaded to base register
+    input wire[31:0] i_load_data_from_csr,      // CSR value to be loaded to basereg
     input wire[`OPCODE_WIDTH-1:0] i_opcode,     // Opcode
+    // Trap Handlers
+    input wire i_go_to_trap,                    // high before going to trap (if exception/interrupt detected)
+    input wire i_return_from_trap,              // high before returning from trap (via MRET)
+    input wire[31:0] i_return_address,          // MEPC CSR
+    input wire[31:0] i_trap_address,            // MTEVC CSR
 
     /* Outputs */
     output reg[31:0] o_rd,                      // Value to be written back to destination register
@@ -53,31 +60,39 @@ module asrv32_writeback #(parameter PC_RESET = 32'h00_00_00_00) (
         wr_rd_d = 0;
         a = o_pc;
         
-        // Set rd_d based on instruction type
-        if(opcode_rtype || opcode_itype) rd_d = i_result_from_alu;
-        if(opcode_load) rd_d = i_load_data_from_memory;
+        // Trap Handling Logic:
+        if(i_go_to_trap) pc_d = i_trap_address;  // Interrupt or exception detected
+        else if(i_return_from_trap) pc_d = i_return_address; // Return from trap via mret
+        // Normal Operation:
+        else begin
+            // Set rd_d based on instruction type
+            if(opcode_rtype || opcode_itype) rd_d = i_result_from_alu;
+            if(opcode_load) rd_d = i_load_data_from_memory;
 
-        // Handle branch instructions
-        if(opcode_branch && i_result_from_alu[0]) pc_d = sum; //branch iff value of ALU is 1(true)
+            // Handle branch instructions
+            if(opcode_branch && i_result_from_alu[0]) pc_d = sum; //branch iff value of ALU is 1(true)
 
-        // Handle JAL and JALR instructions
-        if (opcode_jal || opcode_jalr) begin
-            rd_d = pc_d; // Store next PC in rd
-            pc_d = sum; // Set PC to target address
-            if (opcode_jalr) a = i_rs1_data; 
-        end 
-        if(opcode_jalr) a = i_rs1_data; // For JALR, base address is rs1
+            // Handle JAL and JALR instructions
+            if (opcode_jal || opcode_jalr) begin
+                rd_d = pc_d; // Store next PC in rd
+                pc_d = sum; // Set PC to target address
+                if (opcode_jalr) a = i_rs1_data; 
+            end 
+            if(opcode_jalr) a = i_rs1_data; // For JALR, base address is rs1
 
-        // Handle LUI instruction
-        if (opcode_lui) rd_d = i_imm;
+            // Handle LUI instruction
+            if (opcode_lui) rd_d = i_imm;
 
-        // Handle AUIPC instruction
-        if (opcode_auipc) rd_d = sum;
+            // Handle AUIPC instruction
+            if (opcode_auipc) rd_d = sum;
 
-        // Disable write for certain instruction types
-        if (opcode_branch || opcode_store || opcode_system) wr_rd_d = 0;
-        else wr_rd_d = 1; // Enable write for other instruction types except when instruction is BRANCH/STORE/SYSTEM
-        
+            // Handle CSR Instruction
+            if(opcode_system && i_funct3!=0) rd_d = i_load_data_from_csr;
+
+            // Disable write for certain instruction types
+            if (opcode_branch || opcode_store || opcode_system && i_funct3 == 0) wr_rd_d = 0; // Added i_funct3==0 for non-csr system instructions
+            else wr_rd_d = 1; // Enable write for other instruction types except when instruction is BRANCH/STORE/SYSTEM(Except CSR Instruction)
+        end    
     end
 
     /* Register outputs of this module for shorter combinational timing paths */
