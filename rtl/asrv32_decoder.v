04a2a78502d0e260d71e39fb13bb2a844ceefeea
shaurya0406 <shauryachandra0406@gmail.com>
2024-08-19 16:45:23 +0530

    [UPDATE] Decoder | Added Pipeline signals

--- rtl/asrv32_decoder.v
+++ rtl/asrv32_decoder.v
@@ -8,21 +8,37 @@ module asrv32_decoder
     (
         /* Inputs */
         input wire i_clk,           // Added clock and reset to register the outputs
-        input wire i_rst_n,             
-        input wire[31:0] i_inst,    // The 32-bit instruction to be decoded.
-        
-        /* Outputs */
+        input wire i_rst_n,    
+
+        /* Fetch */         
+        input wire[31:0] i_inst_ifid,    // The 32-bit instruction to be decoded from previous stage (IF)
+        input wire [31:0] i_pc_ifid,
+
+        /* Base RegFile */
         // o_rs1_addr and o_rs2_addr are already registered in the basereg module, hence wire datatype here in decoder definition
         output wire[4:0] o_rs1_addr,    // Address for register source 1. 
         output wire[4:0] o_rs2_addr,    // Address for register source 2.
-        // Leaving o_rd_addr as is for now..
-        output wire[4:0] o_rd_addr,     // Address for the destination register.
-        // Need to hold value for later decoding, hence changed from wire to reg type
-        output reg[31:0] o_imm,         // The sign-extended immediate value extracted from the instruction.
-        output reg[2:0] o_funct3,       // Function type (3-bit field from the instruction). 
-        output reg[`OPCODE_WIDTH-1:0] o_opcode, // Opcode type of the instruction.
-        output reg[`ALU_WIDTH-1:0] o_alu_op,    // ALU operation type.
-        output reg[`EXCEPTION_WIDTH-1:0] o_exception // Exceptions: illegal inst, ecall, ebreak, mret
+
+        /* Execute */
+        output reg[31:0] o_pc_idex,
+        output reg[4:0] o_rs1_addr_idex,
+        output reg[4:0] o_rs2_addr_idex,
+        output reg[4:0] o_rd_addr_idex,     // Address for the destination register.
+        output reg[31:0] o_imm_idex,         // The sign-extended immediate value extracted from the instruction.
+        output reg[2:0] o_funct3_idex,       // Function type (3-bit field from the instruction). 
+        output reg[`OPCODE_WIDTH-1:0] o_opcode_idex, // Opcode type of the instruction.
+        output reg[`ALU_WIDTH-1:0] o_alu_op_idex,    // ALU operation type.
+        output reg[`EXCEPTION_WIDTH-1:0] o_exception_idex, // Exceptions: illegal inst, ecall, ebreak, mret
+
+        /* Pipeline Control */
+        // Forward Propogation
+        input wire i_ce,    // Global clk enable for pipeline stalling from prev stages
+        output reg o_ce,    // Global clk enable for pipeline stalling of next stages
+        // Backward Propogation
+        input wire i_stall, // Global Stall logic for whole pipeline to stall this stage
+        output reg o_stall, // Global Stall logic for whole pipeline to stall prev stages
+        input wire i_flush, // Flush this stage
+        input reg o_flush   // Flush prev stages
 
     );
 
@@ -60,6 +76,9 @@ module asrv32_decoder
     reg valid_opcode = 0;
     reg illegal_shift = 0;
 
+/* Stall Logic for this stage (IF) */
+    wire stall_bit = o_stall || i_stall; //stall this stage when next stages are stalled
+
 /* Functionality Outline: */
 
 /* Operand Address, Opcode & Function Type Extraction: 
@@ -68,12 +87,13 @@ module asrv32_decoder
 */
 
     //o_rs1_addr and o_rs2_addr are not registered since asrv32_basereg module do the registering itself
-    assign o_rs1_addr = i_inst[19:15]; 
-    assign o_rs2_addr = i_inst[24:20];  
-    assign o_rd_addr = i_inst[11:7];
+    assign o_rs1_addr = i_inst_ifid[19:15]; 
+    assign o_rs2_addr = i_inst_ifid[24:20];  
+    // ! 'o_rd_addr' is no longer wire datatype, its a reg, so cant use assign statement
+    // assign o_rd_addr = i_inst_ifid[11:7];
 
-    wire[2:0] funct3_d = i_inst[14:12];
-    wire[6:0] opcode = i_inst[6:0];
+    wire[2:0] funct3_d = i_inst_ifid[14:12];
+    wire[6:0] opcode = i_inst_ifid[6:0];
 
     reg[31:0] imm_d;
 
@@ -103,11 +123,15 @@ module asrv32_decoder
         
         // Check if instruction is illegal    
         valid_opcode = (opcode_rtype_d || opcode_itype_d || opcode_load_d || opcode_store_d || opcode_branch_d || opcode_jal_d || opcode_jalr_d || opcode_lui_d || opcode_auipc_d || opcode_system_d || opcode_fence_d);
-        illegal_shift = (opcode_itype_d && (alu_sll_d || alu_srl_d || alu_sra_d)) && i_inst[25];
+        illegal_shift = (opcode_itype_d && (alu_sll_d || alu_srl_d || alu_sra_d)) && i_inst_ifid[25];
     end
 
     // Decode operation for ALU
     always @* begin
+        // Initialise Peipeline Signals //
+        o_stall = i_stall; // Stall previous stage when decoder needs wait time
+        o_flush = i_flush; // Flush this stage along with the previous stages 
+        // Initialise Decoder Signals //
         imm_d = 0;
         alu_add_d = 0;
         alu_sub_d = 0;
@@ -127,8 +151,8 @@ module asrv32_decoder
         /********** Decode ALU Operation **************/
         if(opcode == `OPCODE_RTYPE || opcode == `OPCODE_ITYPE) begin
             if(opcode == `OPCODE_RTYPE) begin
-                alu_add_d = funct3_d == `FUNCT3_ADD ? !i_inst[30] : 0; // add and sub has same o_funct3 code
-                alu_sub_d = funct3_d == `FUNCT3_ADD ? i_inst[30] : 0;  // differs on i_inst[30]
+                alu_add_d = funct3_d == `FUNCT3_ADD ? !i_inst_ifid[30] : 0; // add and sub has same o_funct3 code
+                alu_sub_d = funct3_d == `FUNCT3_ADD ? i_inst_ifid[30] : 0;  // differs on i_inst_ifid[30]
             end
             else alu_add_d = funct3_d == `FUNCT3_ADD;
 
@@ -138,8 +162,8 @@ module asrv32_decoder
             alu_or_d    = funct3_d == `FUNCT3_OR;
             alu_and_d   = funct3_d == `FUNCT3_AND;
             alu_sll_d   = funct3_d == `FUNCT3_SLL;
-            alu_srl_d   = funct3_d == `FUNCT3_SRA ? !i_inst[30]:0;  // srl and sra has same o_funct3 code differs on i_inst[30]
-            alu_sra_d   = funct3_d == `FUNCT3_SRA ? i_inst[30]:0; 
+            alu_srl_d   = funct3_d == `FUNCT3_SRA ? !i_inst_ifid[30]:0;  // srl and sra has same o_funct3 code differs on i_inst_ifid[30]
+            alu_sra_d   = funct3_d == `FUNCT3_SRA ? i_inst_ifid[30]:0; 
         end
 
         else if(opcode == `OPCODE_BRANCH) begin
@@ -161,12 +185,12 @@ module asrv32_decoder
     always @(*) begin
         imm_d = 0;
         case(opcode)
-        `OPCODE_ITYPE , `OPCODE_LOAD , `OPCODE_JALR: imm_d = {{20{i_inst[31]}},i_inst[31:20]}; 
-                                      `OPCODE_STORE: imm_d = {{20{i_inst[31]}},i_inst[31:25],i_inst[11:7]};
-                                     `OPCODE_BRANCH: imm_d = {{19{i_inst[31]}},i_inst[31],i_inst[7],i_inst[30:25],i_inst[11:8],1'b0};
-                                        `OPCODE_JAL: imm_d = {{11{i_inst[31]}},i_inst[31],i_inst[19:12],i_inst[20],i_inst[30:21],1'b0};
-                        `OPCODE_LUI , `OPCODE_AUIPC: imm_d = {i_inst[31:12],12'h000};
-                     `OPCODE_SYSTEM , `OPCODE_FENCE: imm_d = {20'b0,i_inst[31:20]};   
+        `OPCODE_ITYPE , `OPCODE_LOAD , `OPCODE_JALR: imm_d = {{20{i_inst_ifid[31]}},i_inst_ifid[31:20]}; 
+                                      `OPCODE_STORE: imm_d = {{20{i_inst_ifid[31]}},i_inst_ifid[31:25],i_inst_ifid[11:7]};
+                                     `OPCODE_BRANCH: imm_d = {{19{i_inst_ifid[31]}},i_inst_ifid[31],i_inst_ifid[7],i_inst_ifid[30:25],i_inst_ifid[11:8],1'b0};
+                                        `OPCODE_JAL: imm_d = {{11{i_inst_ifid[31]}},i_inst_ifid[31],i_inst_ifid[19:12],i_inst_ifid[20],i_inst_ifid[30:21],1'b0};
+                        `OPCODE_LUI , `OPCODE_AUIPC: imm_d = {i_inst_ifid[31:12],12'h000};
+                     `OPCODE_SYSTEM , `OPCODE_FENCE: imm_d = {20'b0,i_inst_ifid[31:20]};   
                                             default: imm_d = 0;
         endcase
     end
@@ -249,13 +273,13 @@ module asrv32_decoder
                 o_exception[`ILLEGAL] <= !valid_opcode || illegal_shift;
 
                 // Check if ECALL
-                o_exception[`ECALL] <= (system_noncsr && i_inst[21:20]==2'b00)? 1:0;
+                o_exception[`ECALL] <= (system_noncsr && i_inst_ifid[21:20]==2'b00)? 1:0;
                 
                 // Check if EBREAK
-                o_exception[`EBREAK] <= (system_noncsr && i_inst[21:20]==2'b01)? 1:0;
+                o_exception[`EBREAK] <= (system_noncsr && i_inst_ifid[21:20]==2'b01)? 1:0;
                 
                 // Check if MRET
-                o_exception[`MRET] <= (system_noncsr && i_inst[21:20]==2'b10)? 1:0;
+                o_exception[`MRET] <= (system_noncsr && i_inst_ifid[21:20]==2'b10)? 1:0;
                 /***************************************************************************/
             end
         end
