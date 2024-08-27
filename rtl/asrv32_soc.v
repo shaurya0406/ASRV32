8c299973267bbe853667505aa132e9044b0d1e8b
shaurya0406 <shauryachandra0406@gmail.com>
2024-08-27 10:25:28 +0530

    [UPDATE] SoC | Updated Ports for SoC and Main Memory

--- rtl/asrv32_soc.v
+++ rtl/asrv32_soc.v
@@ -4,17 +4,19 @@
 `include "asrv32_header.vh"
 
 // Complete SoC package containing the main CPU asrv32_core and Main Memory (Combined Instruction & Data Memory)
-module asrv32_soc #(parameter CLK_FREQ_MHZ=100, PC_RESET=32'h00_00_00_00, TRAP_ADDRESS=32'h00_00_00_00, MEMORY_DEPTH=1024) ( 
-    input wire clk, // System clock
-    input wire rst_n, // Active low reset
-    //Interrupts
-    input wire i_external_interrupt, //interrupt from external source
-    input wire i_software_interrupt, //interrupt from software
-    // Timer Interrupt
-    input wire i_mtime_wr, //write to mtime
-    input wire i_mtimecmp_wr,  //write to mtimecmp
-    input wire[63:0] i_mtime_din, //data to be written to mtime
-    input wire[63:0] i_mtimecmp_din //data to be written to mtimecmp
+module asrv32_soc #(parameter CLK_FREQ_MHZ=100, PC_RESET=32'h00_00_00_00, TRAP_ADDRESS=32'h00_00_00_00, ZICSR_EXTENSION=1, MEMORY_DEPTH=1024) ( 
+    input wire i_clk, // System clock
+    input wire i_rst_n, // Active low reset
+
+    // ! Moved to VIC Module
+    // //Interrupts
+    // input wire i_external_interrupt, //interrupt from external source
+    // input wire i_software_interrupt, //interrupt from software
+    // // Timer Interrupt
+    // input wire i_mtime_wr, //write to mtime
+    // input wire i_mtimecmp_wr,  //write to mtimecmp
+    // input wire[63:0] i_mtime_din, //data to be written to mtime
+    // input wire[63:0] i_mtimecmp_din //data to be written to mtimecmp
     );
     
     // Instruction Memory Interface
@@ -27,11 +29,18 @@ module asrv32_soc #(parameter CLK_FREQ_MHZ=100, PC_RESET=32'h00_00_00_00, TRAP_A
     wire[31:0] daddr; // Address of data memory for store/load
     wire[3:0] wr_mask; // Write mask control
     wire wr_en; // Write enable 
+    wire i_stb_data;
+    wire o_ack_data;
+
+    // Interrupts
+    wire i_external_interrupt = 0; // interrupt from external source
+    wire o_timer_interrupt; // interrupt from VIC
+    wire o_software_interrupt; // interrupt from VIC
 
     // Main ASRV32 core instantiation
     asrv32_core #(.PC_RESET(PC_RESET),.CLK_FREQ_MHZ(CLK_FREQ_MHZ), .TRAP_ADDRESS(TRAP_ADDRESS)) m0( 
-        .i_clk(clk), // System clock
-        .i_rst_n(rst_n), // Active low reset
+        .i_clk(i_clk), // System clock
+        .i_rst_n(i_rst_n), // Active low reset
         // Instruction Memory Interface
         .i_inst(inst), // 32-bit instruction
         .o_inst_addr(iaddr), // Address of instruction 
@@ -53,7 +62,7 @@ module asrv32_soc #(parameter CLK_FREQ_MHZ=100, PC_RESET=32'h00_00_00_00, TRAP_A
         
     // Main memory instantiation
     main_memory #(.MEMORY_DEPTH(MEMORY_DEPTH)) m1(
-        .i_clk(clk), // System clock
+        .i_clk(i_clk), // System clock
         // Instruction Memory Interface
         .i_inst_addr(iaddr[$clog2(MEMORY_DEPTH)-1:0]), // Instruction address
         .o_inst_out(inst), // Instruction output
@@ -74,11 +83,15 @@ module main_memory #(parameter MEMORY_DEPTH=1024) (
     // Instruction Memory Interface
     input wire[$clog2(MEMORY_DEPTH)-1:0] i_inst_addr, // Instruction address
     output wire[31:0] o_inst_out, // Instruction output
+    input wire i_stb_inst, // request for instruction
+    output reg o_ack_inst, //ack (high if new instruction is now on the bus)
     // Data Memory Interface
     input wire[$clog2(MEMORY_DEPTH)-1:0] i_data_addr, // Data address
     input wire[31:0] i_data_in, // Data input
     input wire[3:0] i_wr_mask, // Write mask
     input wire i_wr_en, // Write enable
+    input wire i_stb_data,
+    output reg o_ack_data,
     output wire[31:0] o_data_out // Data output
 );
     // Memory array for storing instructions and data
@@ -87,8 +100,12 @@ module main_memory #(parameter MEMORY_DEPTH=1024) (
     
     // Initialize memory to zero
     initial begin 
-        for(i = 0; i < MEMORY_DEPTH/4 - 1; i = i + 1) 
-            memory_regfile[i] = 0; 
+        // for(i = 0; i < MEMORY_DEPTH/4 - 1; i = i + 1) 
+        //     memory_regfile[i] = 0; 
+        $readmemh("memory.mem",memory_regfile);
+        o_ack_inst <= 0;
+        o_ack_data <= 0;
+        o_inst_out <= 0;
     end
     
     // Read instruction from memory
