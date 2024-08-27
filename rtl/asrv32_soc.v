3e433aee047094b0f2c33a0135fff86cdd1fa853
shaurya0406 <shauryachandra0406@gmail.com>
2024-08-27 10:37:54 +0530

    [UPDATE] SoC | Added VIC

--- rtl/asrv32_soc.v
+++ rtl/asrv32_soc.v
@@ -4,7 +4,7 @@
 `include "asrv32_header.vh"
 
 // Complete SoC package containing the main CPU asrv32_core and Main Memory (Combined Instruction & Data Memory)
-module asrv32_soc #(parameter CLK_FREQ_MHZ=100, PC_RESET=32'h00_00_00_00, TRAP_ADDRESS=32'h00_00_00_00, ZICSR_EXTENSION=1, MEMORY_DEPTH=1024) ( 
+module asrv32_soc #(parameter CLK_FREQ_MHZ=12, PC_RESET=32'h00_00_00_00, TRAP_ADDRESS=32'h00_00_00_00, ZICSR_EXTENSION=1, MEMORY_DEPTH=1024) ( 
     input wire i_clk, // System clock
     input wire i_rst_n, // Active low reset
 
@@ -119,7 +119,7 @@ module main_memory #(parameter MEMORY_DEPTH=1024) (
         // Read data from memory, sent out in next clock cycle
         o_data_out <= memory_regfile[i_data_addr[$clog2(MEMORY_DEPTH)-1:2]]; 
     end
-    
+
     // Write data to memory
     always @(posedge i_clk) begin
         if(i_wr_en) begin
@@ -130,4 +130,94 @@ module main_memory #(parameter MEMORY_DEPTH=1024) (
         end        
     end
     
+endmodule
+
+
+
+module asrv32_vic #( // Vectored Interrupt Controller
+    parameter CLK_FREQ_MHZ = 12, //input clock frequency in MHz
+
+    // * A MTIMER device has two separate base addresses: one for the MTIME register and another for the MTIMECMP registers. 
+    parameter MTIME_BASE_ADDRESS = 8008,
+              MTIMECMP_BASE_ADDRESS = 8016,
+              MSIP_BASE_ADDRESS = 8024
+)(
+        input wire clk,
+        input wire rst_n,
+        input wire[31:0] clint_address, // read/write address (access the memory-mapped registers for controlling i2c)
+        input wire[31:0] clint_wdata, // data to be written to slave or to memory-mapped registers of i2c
+        output reg[31:0] clint_rdata, // data retrieved from slave or from the memory-mapped registers of i2c
+        input wire clint_wr_en, // write-enable
+        input wire i_stb, // request to access CLINT
+        output reg o_ack, // acknowledge by CLINT
+
+        // Interrupts
+        output wire o_timer_interrupt,
+        output wire o_software_interrupt
+);
+    // ? This is based from RISC-V Advanced Core Local Interruptor
+    // ? Specification: https://github.com/riscv/riscv-aclint/blob/main/riscv-aclint.adoc
+
+    // ? This RISC-V ACLINT specification defines a set of memory mapped devices which provide 
+    // ? inter-processor interrupts (IPI) and timer functionalities.
+    // ? The MTIMER device provides machine-level timer functionality for a set of HARTs on a RISC-V platform. 
+    // ? It has a single fixed-frequency monotonic time counter (MTIME) register and a time 
+    // ? compare register (MTIMECMP) for each HART connected to the MTIMER device.
+    
+    reg[63:0] mtime = 0;
+    reg[63:0] mtimecmp = {64{1'b1}};   
+    reg msip = 0; //Inter-processor (or software) interrupts
+
+
+   //READ memory-mapped registers 
+    always @(posedge clk, negedge rst_n) begin
+        if(!rst_n) begin
+            o_ack <= 0;
+            clint_rdata <= 0;
+        end
+        else begin
+            if(i_stb && !clint_wr_en) begin //read the memory-mapped register
+                if(clint_address == MTIME_BASE_ADDRESS) clint_rdata <= mtime[31:0]; //first half 
+                else if(clint_address == MTIME_BASE_ADDRESS + 4) clint_rdata <= mtime[63:32]; //second half
+                if(clint_address == MTIMECMP_BASE_ADDRESS) clint_rdata <= mtimecmp[31:0]; //first half
+                else if(clint_address == MTIMECMP_BASE_ADDRESS + 4) clint_rdata <= mtimecmp[63:32]; //second half
+                if(clint_address == MSIP_BASE_ADDRESS) clint_rdata <= {31'b0, msip}; //machine software interrupt
+            end
+            o_ack <= i_stb; //wishbone protocol stb-ack mechanism
+        end
+    end
+
+    //WRITE to memory-mapped registers 
+    always @(posedge clk, negedge rst_n) begin
+        if(!rst_n) begin
+            mtime <= 64'd0;
+            mtimecmp <= {64{1'b1}}; //timer interrupt will be triggered unintentionally if reset at 0 (equal to mtime) 
+                                  //thus we set it at highest value (all 1s)
+            msip <= 0;
+        end
+        else begin
+            if(i_stb && clint_wr_en) begin //write to the memory-mapped registers
+                if(clint_address == MTIME_BASE_ADDRESS)  mtime[31:0] <= clint_wdata; //first half 
+                else if(clint_address == MTIME_BASE_ADDRESS + 4) mtime[63:32] <= clint_wdata; //second half
+                if(clint_address == MTIMECMP_BASE_ADDRESS) mtimecmp[31:0] <= clint_wdata; //first half
+                else if(clint_address == MTIMECMP_BASE_ADDRESS + 4) mtimecmp[63:32] <= clint_wdata; //second half
+                if(clint_address == MSIP_BASE_ADDRESS) msip <= clint_wdata[0]; //machine software interrupt
+            end
+            mtime <= mtime + 1'b1; //increment every clock tick (so timer freq is same as cpu clock freq)
+        end
+    end
+
+    //? Volume 2 pg. 44: Platforms provide a 64-bit memory-mapped machine-mode timer compare register (mtimecmp). 
+    //? A machine timer interrupt becomes pending whenever mtime contains a value greater than or equal to mtimecmp, 
+    //? treating the values as unsigned integers. The interrupt remains posted until mtimecmp becomes greater than
+    //? mtime (typically as a result of writing mtimecmp). 
+    
+    assign o_timer_interrupt = (mtime >= mtimecmp);
+
+    //? Each MSIP register is a 32-bit wide WARL register where the upper 31 bits are wired to zero.
+    //? The least significant bit is reflected in MSIP of the mip CSR. A machine-level software interrupt 
+    //? for a HART is pending or cleared by writing 1 or 0 respectively to the corresponding MSIP register.
+    
+     assign o_software_interrupt = msip;
+
 endmodule
