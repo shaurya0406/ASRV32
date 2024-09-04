eaa2afb3295e05de9abd056fbc7441c2808389d4
shaurya0406 <shauryachandra0406@gmail.com>
2024-09-04 11:47:53 +0530

    [UPDATE] SoC | I2C Module

--- rtl/asrv32_soc.v
+++ rtl/asrv32_soc.v
@@ -607,3 +607,249 @@ module uart #( //UART (TX only)
 	 /*********************************************************************************/
 	 
 endmodule
+
+
+module i2c //SCCB mode(no pullups resistors needed) [REPEATED START NOT SUPPORTED]
+    #(parameter main_clock=12_000_000, //frequency of clk
+                freq=100_000, //i2c freqeuncy
+                addr_bytes=2,//addr_bytes=number of bytes of an address
+                I2C_START=8100, //write-only memory-mapped address to start i2c (write the i2c slave address)
+                I2C_WRITE=8104, //write-only memory-mapped address for sending data to slave
+                I2C_READ=8108, //read-only memory-mapped address to read data received from slave (this will also continue reading from slave) 
+                I2C_BUSY=8112, //read-only memory-mapped address to check if i2c is busy (cannot accept request)
+                I2C_ACK=8116, //read-only memory-mapped address to check if last access has benn acknowledge by slave
+                I2C_READ_DATA_READY=8120, //read-only memory-mapped address to check if data to be received from slave is ready
+                I2C_STOP=8124 //write-only memory-mapped address to stop i2c (this is persistent thus must be manually turned off after stopping i2c)
+    ) 
+    (
+        input   wire        clk,
+        input   wire        rst_n,
+        input   wire [31:0] i2c_rw_address, //read/write address (access the memory-mapped registers for controlling i2c)
+        input   wire [7:0 ] i2c_wdata, //data to be written to slave or to memory-mapped registers of i2c
+        output reg [7:0] i2c_rdata, //data retrieved from slave or from the memory-mapped registers of i2c
+        input   wire        i2c_wr_en, //write-enable
+        inout wire scl, sda, //i2c bidrectional clock and data line
+        output reg o_ack_data, //acknowledge by i2c
+        input wire i_stb_data //request to access i2c
+    ); 
+     
+
+    //memory-mapped registers for controlling i2c
+    wire[7:0] i2c_busy = {7'b0, !((state_q == idle) || (state_q == stop_or_write) || (state_q == stop_or_read))}; //check if busy (busy unless we are on these states)
+    wire[7:0] i2c_read_data_ready = {7'b0, (state_q == stop_or_read)}; //check if data is ready to be read (data is ready ONLY WHEN we are already waiting for another read request!)
+    reg[7:0] i2c_ack; //check last access request has been acknowledged by slave
+    reg[7:0] i2c_stop; //write non-zero data here to stop current read/write transaction
+
+    
+    wire start = i_stb_data;
+    wire[7:0] wr_data = i2c_wdata;
+    reg ack;
+    reg rd_tick;
+
+     localparam full= (main_clock)/(2*freq),
+                    half= full/2,           
+                    counter_width=$clog2(full);
+         
+     //FSM state declarations
+    localparam[3:0] idle=0,
+                    starting=1,
+                    packet=2,
+                    ack_servant=3,
+                    read=4,
+                    ack_master=5,
+                    stop_1=6,
+                    stop_2=7,
+                    stop_or_read = 8,
+                    stop_or_write = 9;
+    reg[3:0] state_q=idle,state_d;
+     reg op_q=0,op_d;
+     reg[3:0] idx_q=0,idx_d;
+     reg[8:0] wr_data_q=0,wr_data_d;
+     reg[7:0] rd_data_q,rd_data_d;
+     reg scl_q=0,scl_d;
+     reg sda_q=0,sda_d;
+     reg[counter_width-1:0] counter_q=0,counter_d;
+     reg[1:0] addr_bytes_q=0,addr_bytes_d;
+     wire scl_lo,scl_hi;
+     wire sda_in, sda_out;
+    
+    //access memory-mapped register
+    always @(posedge clk, negedge rst_n) begin
+        if(!rst_n) begin
+            i2c_stop <= 0;
+            o_ack_data <= 0;
+        end
+        else begin
+            if(i_stb_data && i2c_wr_en && i2c_rw_address == I2C_STOP) i2c_stop <= i2c_wdata; //write to i2c_stop to stop transaction
+            if(i_stb_data && !i2c_wr_en && i2c_rw_address == I2C_ACK) i2c_rdata <= i2c_ack; //read i2c_ack to know if last access request has been ack by slave
+            if(i_stb_data && !i2c_wr_en && i2c_rw_address == I2C_READ_DATA_READY) i2c_rdata <= i2c_read_data_ready;//read this to know if data is ready to be read
+            if(i_stb_data && !i2c_wr_en && i2c_rw_address == I2C_BUSY) i2c_rdata <= i2c_busy; //read this to know if i2c is still busy
+            if(i_stb_data && !i2c_wr_en && i2c_rw_address == I2C_READ) i2c_rdata <= rd_data_q; //read this to know what has been read from slave (make sure I2C_READ_DATA_READY is already high) 
+
+            o_ack_data <= i_stb_data; 
+        end
+    end
+
+    //register operations
+     always@(posedge clk, negedge rst_n) begin
+        if(!rst_n) begin
+            state_q<=idle;
+            idx_q<=0;
+            wr_data_q<=0;
+            scl_q<=0;
+            sda_q<=0;
+            counter_q<=0;
+            rd_data_q<=0;
+            addr_bytes_q<=0;
+            i2c_ack <= 0;
+        end
+        else begin
+            state_q<=state_d;
+            op_q<=op_d;
+            idx_q<=idx_d;
+            wr_data_q<=wr_data_d;
+            scl_q<=scl_d;
+            sda_q<=sda_d;
+            if(i2c_busy[0]) counter_q<=counter_d; //freeze the scl (by freezing the counter) if we are on wait/idle state (not busy states)
+            rd_data_q<=rd_data_d;
+            addr_bytes_q<=addr_bytes_d;
+            i2c_ack <= {7'd0,ack};
+        end
+     end
+     
+     
+     //free-running clk, freq depends on parameter "freq"
+     always @* begin
+        counter_d=counter_q+1;
+        scl_d=scl_q;
+        if(state_q==idle || state_q==starting) scl_d=1'b1;
+        else if(counter_q==full[counter_width-1:0]) begin
+            counter_d=0;
+            scl_d=(scl_q==0)?1'b1:1'b0;
+        end
+     end
+     
+    //I2C_START 
+     //FSM next-state logic
+     always @* begin
+        state_d=state_q;
+        op_d=op_q;
+        idx_d=idx_q;
+        wr_data_d=wr_data_q;
+        rd_data_d=rd_data_q;
+        addr_bytes_d=addr_bytes_q;
+        sda_d=sda_q;
+        rd_tick=0;
+        ack=i2c_ack[0];
+        case(state_q)
+                    idle: begin //wait for user to start i2c by writing the slave address to I2C_START
+                                sda_d=1'b1;
+                                addr_bytes_d=addr_bytes; 
+                                if(start==1'b1 && i2c_wr_en && i2c_rw_address == I2C_START) begin //wait for a request
+                                    wr_data_d={wr_data,1'b1}; //the last 1'b1 is for the ACK coming from the servant("1" means high impedance or "reading")
+                                    op_d= (wr_data[0])? 1:0; // if last bit(R/W bit) is one:read operation, else write operation
+                                    idx_d=8; //index to be used on transmitting the wr_data serially(MSB first)
+                                    state_d=starting;
+                                end
+                             end
+                             
+                starting: if(scl_hi) begin //start command, change sda to low while scl is high
+                                sda_d=0;
+                                state_d=packet;
+                             end
+                             
+                  packet: if(scl_lo) begin //transmit wr_data serially(MSB first)
+                                sda_d= (wr_data_q[idx_q]==0)? 0:1'b1;
+                                idx_d= idx_q-1;
+                                if(idx_q==0) begin
+                                    state_d=ack_servant;
+                                    idx_d=0;
+                                end
+                             end
+                             
+            ack_servant: if(scl_hi) begin //wait for ACK bit response(9th bit) from servant
+                                ack=!sda_in; 
+                                if(i2c_stop[0]) state_d=stop_1; //master can forcefully stops the transaction (i2c_stop is memory-mapped)
+                                else if(op_q/* && addr_bytes_q==0*/) begin //start reading after writing "addr_bytes" of packets for address
+                                    idx_d=7;
+                                    state_d=read;
+                                end
+                                else begin //write next packet
+                                    state_d = stop_or_write;
+                                    idx_d=8;
+                                end
+                             end
+                             
+               stop_or_write:  if(i2c_stop[0]) begin //wait until user explicitly say to either stop i2c or continue writing
+                                state_d = stop_1;
+                            end
+                            else if(start && i2c_wr_en && i2c_rw_address == I2C_WRITE) begin//continue writing                   
+                                    state_d = packet;
+                                    wr_data_d={wr_data,1'b1}; 
+                                    addr_bytes_d=addr_bytes_q-1;
+                            end
+
+                     read: if(scl_hi) begin //read data from slave(MSB first)
+                                rd_data_d[idx_q[2:0]]=sda_in;
+                                idx_d=idx_q-1;
+                                if(idx_q==0) state_d=ack_master;
+                             end
+                             
+             ack_master: if(scl_lo) begin //master must ACK after receiving data from servant
+                                sda_d=1'b0; 
+                                if(sda_q==0) begin //one whole bit(two scl_lo) had passed
+                                    rd_tick=1;
+                                    idx_d=7;
+                                    if(i2c_stop[0]) state_d=stop_1; //after receiving data, master can opt to stop
+                                    else state_d=stop_or_read;
+                                end
+                             end
+            stop_or_read: if(i2c_stop[0]) begin //wait until user explicitly say to either stop i2c or continue reading
+                             state_d = stop_1;
+                         end
+                         else if(start && !i2c_wr_en && i2c_rw_address == I2C_READ) begin //continue reading when current data is read
+                             state_d = read;
+                         end
+
+                  stop_1: if(scl_lo) begin 
+                                sda_d=1'b0;
+                                state_d=stop_2;
+                             end
+                  stop_2: if(scl_hi) begin
+                                sda_d=1'b1;
+                                state_d=idle;
+                             end
+                 default: state_d=idle;
+        endcase
+     end
+     
+     //i2c IO logic requires pull-ups (2 logic levels: 0 or Z)
+     //assign scl=scl_q? 1'bz:0; //bidiectional logic for pull-up scl
+     //assign sda=sda_q? 1'bz:0; //bidirectional logic for pull-up scl
+     //assign sda_in=sda;
+     
+     //We don't used pull-ups here so logic can be 0 or 1 (instead of high
+     //impedance). This is similar to SCCB protocol. 
+     wire is_reading;
+     assign is_reading = (state_q==read || state_q==ack_servant);
+     assign sda_out = sda_q;
+    
+    //Vivado, use IOBUF primitive
+    `ifndef ICARUS 
+     IOBUF sda_iobuf ( //Vivado IOBUF instantiation
+            .IO(sda),
+            .I(sda_out),//write SDA when is_reading low
+            .T(is_reading), 
+            .O(sda_in) //read SDA when is_reading high
+        );
+     `endif
+     //Icarus simulator
+    `ifdef ICARUS
+        assign sda = sda_q;
+    `endif
+
+    assign scl = scl_q;
+    assign scl_hi= scl_q==1'b1 && counter_q==half[counter_width-1:0] /*&& scl==1'b1*/; //scl is on the middle of a high(1) bit
+    assign scl_lo= scl_q==1'b0 && counter_q==half[counter_width-1:0]; //scl is on the middle of a low(0) bit
+
+endmodule
