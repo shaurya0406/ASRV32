160c435a4db0c749685a8f64a1a0fc23ac50d472
shaurya0406 <shauryachandra0406@gmail.com>
2024-09-04 11:47:17 +0530

    [UPDATE] SoC | UART Module

--- rtl/asrv32_soc.v
+++ rtl/asrv32_soc.v
@@ -4,9 +4,17 @@
 `include "asrv32_header.vh"
 
 // Complete SoC package containing the main CPU asrv32_core and Main Memory (Combined Instruction & Data Memory)
-module asrv32_soc #(parameter CLK_FREQ_MHZ=12, PC_RESET=32'h00_00_00_00, TRAP_ADDRESS=32'h00_00_00_00, ZICSR_EXTENSION=1, MEMORY_DEPTH=1024) ( 
+module asrv32_soc #(parameter CLK_FREQ_MHZ=12, PC_RESET=32'h00_00_00_00, TRAP_ADDRESS=32'h00_00_00_00, ZICSR_EXTENSION=1, MEMORY_DEPTH=1024, GPIO_COUNT = 4) ( 
     input wire i_clk, // System clock
     input wire i_rst // Active low reset
+    //UART
+    input wire uart_rx,
+    output wire uart_tx,
+    //I2C
+    inout wire i2c_sda,
+    inout wire i2c_scl,
+    //GPIO
+    inout wire[GPIO_COUNT-1:0] gpio_pins
     );
     
     // Instruction Memory Interface
@@ -357,4 +365,245 @@ module peripheral_bus_controller ( // Decodes address and access the correspondi
             o_ack_data = i_device0_ack_data;
         end
     end   
+endmodule
+
+
+
+module uart #( //UART (TX only)
+    parameter CLOCK_FREQ = 12_000_000,//Input clock frequency
+    parameter BAUD_RATE  = 9600, //UART Baud rate
+    parameter UART_TX_DATA = 8140, //memory-mapped address for TX (write to UART)
+    parameter UART_TX_BUSY = 8144, //memory-mapped address to check if TX is busy (has ongoing request)
+    parameter UART_RX_BUFFER_FULL = 8148, //memory-mapped address  to check if a read has completed
+    parameter UART_RX_DATA = 8152, //memory-mapped address for RX (read the data)
+    parameter DBIT = 8, //UART Data Bits
+    parameter SBIT = 1 //UART Stop Bits
+    )(
+        input wire clk,
+        input wire rst_n,
+        input wire[31:0] uart_rw_address, //read/write address (access the memory-mapped registers for controlling UART)
+        input wire[DBIT - 1:0 ] uart_wdata, //TX data
+        input wire uart_wr_en, //write-enable
+        input wire uart_rx, //UART RX line
+        output wire uart_tx, //UART TX line
+        output reg[DBIT - 1:0] uart_rdata, //data read from memory-mapped register 
+        input wire i_stb_data, //request to access UART
+        output reg o_ack_data //acknowledge by UART
+    );
+
+
+    localparam DVSR = CLOCK_FREQ/(16*BAUD_RATE);
+    localparam DVSR_WIDTH = $clog2(DVSR); //array size needed by DVSR
+    localparam SB_TICK = 16*SBIT;
+    
+     //FSM state declarations
+     localparam[1:0] idle=2'd0,
+                    start=2'd1,
+                    data=2'd2,
+                    stop=2'd3;
+                    
+    reg[DBIT - 1:0] uart_busy;    
+    reg tx_done_tick;          
+    reg[1:0] state_reg,state_nxt;
+    reg[3:0] s_reg,s_nxt; //count to 16 for every data bit
+    reg[2:0] n_reg,n_nxt; //count the number of data bits already transmitted
+    reg[DBIT - 1:0] din_reg,din_nxt; //stores the word to be transmitted
+    reg tx_reg,tx_nxt;
+    reg s_tick;
+    reg wr_uart;
+    reg[1:0] state_reg_rx,state_nxt_rx;
+    reg[3:0] s_reg_rx,s_nxt_rx; //check if number of ticks is 7(middle of start bit), or 15(middle of a data bit)
+    reg[2:0] n_reg_rx,n_nxt_rx; //checks how many data bits is already passed(value is 7 for last bit)
+    reg[7:0] b_reg,b_nxt; //stores 8-bit binary value of received data bits
+    reg[7:0] dout; //data read from UART
+    reg rx_done_tick; //goes high if a read is done
+    reg rx_buffer_full; //goes high if a read is done
+
+
+    //baud tick generator
+     reg[DVSR_WIDTH-1:0] counter=0;
+     always @(posedge clk,negedge rst_n) begin
+        if(!rst_n) counter<=0;
+        else begin
+            s_tick=0;
+            if(counter == DVSR-1) begin
+                s_tick=1;
+                counter<=0;
+            end
+            else begin
+                counter<=counter+1;
+            end
+            
+        end
+     end
+     //Read memory-mapped registers
+     always @(posedge clk, negedge rst_n) begin
+        if(!rst_n) begin
+            uart_rdata <= 0;
+            o_ack_data <= 0;
+        end
+        else begin
+            if(i_stb_data && !uart_wr_en && uart_rw_address == UART_TX_BUSY) begin //read request to UART_TX_BUSY_ADDR (check if there is an ongoing request)
+                uart_rdata <= uart_busy;
+            end
+            else if(i_stb_data && !uart_wr_en && uart_rw_address == UART_RX_BUFFER_FULL) begin //read request to UART_RX_BUFFER_FULL (check if a read is completed)
+                uart_rdata <= rx_buffer_full;
+            end
+            else if(i_stb_data && !uart_wr_en && uart_rw_address == UART_RX_DATA) begin //read request to UART_RX_DATA (read the data)
+                uart_rdata <= dout;
+            end
+            o_ack_data <= i_stb_data;
+        end
+     end
+     
+     
+     /******************************** UART TX ****************************************/
+
+    
+     //FSM register operation
+     always @(posedge clk,negedge rst_n) begin
+        if(!rst_n) begin
+            state_reg<=idle;
+            s_reg<=0;
+            n_reg<=0;
+            din_reg<=0;
+            tx_reg<=0;
+        end
+        else begin
+            state_reg<=state_nxt;
+            s_reg<=s_nxt;
+            n_reg<=n_nxt;
+            din_reg<=din_nxt;
+            tx_reg<=tx_nxt;
+        end
+     end
+     
+     //FSM next-state logic
+     always @* begin
+        state_nxt=state_reg;
+        s_nxt=s_reg;
+        n_nxt=n_reg;
+        din_nxt=din_reg;
+        tx_nxt=tx_reg;
+        tx_done_tick=0; 
+        uart_busy= 1; //uart is busy unless its in idle state
+         case(state_reg)
+                idle: begin 
+                            tx_nxt=1;
+                            uart_busy = 0;
+                            //start transmit operation when there is a write request to UART_TX_DATA_ADDR and we are in idle
+                            if(uart_wr_en && i_stb_data && uart_rw_address == UART_TX_DATA && !uart_busy) begin 
+                                din_nxt=uart_wdata;
+                                s_nxt=0;
+                                state_nxt=start;
+                                uart_busy = 1;
+                            end
+                        end
+              start: begin   //wait to finish the start bit
+                            tx_nxt=0;
+                            if(s_tick==1) begin
+                                if(s_reg==15) begin
+                                    s_nxt=0;
+                                    n_nxt=0;
+                                    state_nxt=data;
+                                end
+                                else s_nxt=s_reg+1;
+                            end
+                        end
+                data: begin  //wait for all data bits to be transmitted serially
+                            tx_nxt=din_reg[0];
+                            if(s_tick==1) begin
+                                if(s_reg==15) begin
+                                    din_nxt=din_reg>>1;
+                                    s_nxt=0;
+                                    if(n_reg==DBIT-1) state_nxt=stop;
+                                    else n_nxt=n_reg+1;
+                                end
+                                else s_nxt=s_reg+1;
+                            end
+                        end
+                stop: begin  //wait to finish the stop bit 
+                            tx_nxt=1;
+                            if(s_tick==1) begin
+                                if(s_reg==SB_TICK-1) begin
+                                    tx_done_tick=1;
+                                    state_nxt=idle;
+                                end
+                                else s_nxt=s_reg+1;
+                            end
+                        end
+            default: state_nxt=idle;
+         endcase
+     end
+     assign uart_tx=tx_reg;
+    /*********************************************************************************/
+    
+    /******************************** UART RX ****************************************/
+	 
+	 //FSM register operation
+	 always @(posedge clk,negedge rst_n) begin
+		if(!rst_n) begin
+			state_reg_rx<=idle;
+			s_reg_rx<=0;
+			n_reg_rx<=0;
+			b_reg<=0;
+			dout<=0;
+			rx_buffer_full<=0;
+		end
+		else begin
+			state_reg_rx<=state_nxt_rx;
+			s_reg_rx<=s_nxt_rx;
+			n_reg_rx<=n_nxt_rx;
+			b_reg<=b_nxt;	
+			if(rx_done_tick) begin
+			    dout <= b_reg; //memory-mapped register storing the completed read data	
+			    rx_buffer_full <= 1'b1; //memory-mapped register to check if a read is done
+			end
+			else if(i_stb_data && !uart_wr_en && uart_rw_address == UART_RX_DATA) begin //read request to UART_RX_DATA (read the data)
+                rx_buffer_full <= 1'b0;
+            end
+		end
+	 end
+	 
+	 //FSM next-state logic
+	 always @* begin
+		state_nxt_rx=state_reg_rx;
+		s_nxt_rx=s_reg_rx;
+		n_nxt_rx=n_reg_rx;
+		b_nxt=b_reg;
+		rx_done_tick=0;
+		case(state_reg_rx)
+			 idle: if(uart_rx==0) begin //wait for start bit(rx of zero)
+						s_nxt_rx=0;
+						state_nxt_rx=start;
+					 end						
+			start: if(s_tick==1) begin //wait for middle of start bit
+						if(s_reg_rx==7) begin
+							s_nxt_rx=0;
+							n_nxt_rx=0;
+							state_nxt_rx=data;
+						end
+						else s_nxt_rx=s_reg_rx+1;
+					 end
+		    data: if(s_tick==1) begin //wait to pass all middle points of every data bits
+						if(s_reg_rx==15) begin
+							b_nxt={uart_rx,b_reg[7:1]};
+							s_nxt_rx=0;
+							if(n_reg_rx==DBIT-1) state_nxt_rx=stop;
+							else n_nxt_rx=n_reg_rx+1;
+						end
+						else s_nxt_rx=s_reg_rx+1;
+					 end
+			 stop: if(s_tick==1) begin  //wait to pass the required stop bits
+						if(s_reg_rx==SB_TICK-1) begin
+							rx_done_tick=1;
+							state_nxt_rx=idle;
+						end
+  						else s_nxt_rx=s_reg_rx+1;
+					 end	
+		 default: state_nxt_rx=idle;
+		endcase
+	 end
+	 /*********************************************************************************/
+	 
 endmodule
